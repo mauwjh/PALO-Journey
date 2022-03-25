0d7e927e977e4758ad9ae52a445515f5bf43950a
mauwjh <matthew.auw@gmail.com>
2022-03-25 15:39:43 +0800

    debug overflow mobile

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ const InputLine = ({
         >
           <AutoFocusInside>
             <Input
-              style={{maximumScale: "1"}}
+              style={{ maximumScale: "1" }}
               color="white"
               variant="unstyled"
               onChange={(event) => {
@@ -361,164 +361,131 @@ export default function Home() {
   };
 
   return (
-    <>
-      <Flex
-        h="100vh"
-        w="100vw"
-        bg="linear-gradient(90deg, rgba(5,204,152,1) 0%, rgba(255,175,59,1) 53%, rgba(239,128,53,1) 100%)"
-        align="center"
-        justify="center"
-        display={{ base: "none", lg: "flex" }}
-      >
-        <Flex direction="column" w="70%" h="70%">
-          <Flex
-            bg="#404040"
-            minH="30px"
-            h="30px"
-            w="100%"
-            borderRadius="10px 10px 0 0 "
-            align="center"
-            justify="space-between"
-          >
-            <Flex minW="33%">
-              <Box
-                borderRadius="50%"
-                w="15px"
-                h="15px"
-                bg="#FF605C"
-                ml="10px"
-                mr="5px"
-              />
-              <Box
-                borderRadius="50%"
-                w="15px"
-                h="15px"
-                bg="#FFBD44"
-                ml="5px"
-                mr="5px"
-              />
-              <Box
-                borderRadius="50%"
-                w="15px"
-                h="15px"
-                bg="#00CA4E"
-                ml="5px"
-                mr="5px"
-              />
-            </Flex>
-            <Text
-              w="33%"
-              textAlign="center"
-              color="#a6a2a2"
-              fontWeight="bold"
-              fontFamily="Arial"
-            >
-              Matt's Probation Journey
-            </Text>
-            <Text w="33%" />
+    <Flex
+      h="100vh"
+      w="100vw"
+      bg="linear-gradient(90deg, rgba(5,204,152,1) 0%, rgba(255,175,59,1) 53%, rgba(239,128,53,1) 100%)"
+      align="center"
+      justify="center"
+    >
+      <Flex direction="column" w={{ base: "100%", lg: "70%" }} h={{ base: "100%", lg: "70%" }}>
+        <Flex
+          bg="#404040"
+          minH="30px"
+          h="30px"
+          w="100%"
+          borderRadius="10px 10px 0 0"
+          align="center"
+          justify="space-between"
+          display={{ base: "none", lg: "flex" }}
+        >
+          <Flex minW="33%">
+            <Box
+              borderRadius="50%"
+              w="15px"
+              h="15px"
+              bg="#FF605C"
+              ml="10px"
+              mr="5px"
+            />
+            <Box
+              borderRadius="50%"
+              w="15px"
+              h="15px"
+              bg="#FFBD44"
+              ml="5px"
+              mr="5px"
+            />
+            <Box
+              borderRadius="50%"
+              w="15px"
+              h="15px"
+              bg="#00CA4E"
+              ml="5px"
+              mr="5px"
+            />
           </Flex>
-          <Flex
-            w="100%"
-            grow="1"
-            bg="#262626"
-            borderRadius="0 0 10px 10px"
-            direction="column"
-            overflow="hidden"
+          <Text
+            w="33%"
+            textAlign="center"
+            color="#a6a2a2"
+            fontWeight="bold"
+            fontFamily="Arial"
           >
-            <Box overflowY="scroll" w="100%" h="100%" p="10px">
-              <Typist cursor={{ show: false }}>
-                <Text>Welcome!</Text>
-                <br />
-                <Text>Type 'help' to view a list of commands</Text>
-                <Text>Or hit [Tab] to cycle through them</Text>
-              </Typist>
-              <br />
-              {render.map((x, i) => (
-                <Box key={x + i}>{x}</Box>
-              ))}
-              <InputLine
-                input={input}
-                setInput={setInput}
-                handleSubmit={(e) => handleSubmit(e)}
-                setVisible={setVisible}
-                visible={visible}
-                options={Object.keys(content)}
-              />
-              <Box ref={endOfPage} />
-            </Box>
-          </Flex>
+            Matt's Probation Journey
+          </Text>
+          <Text w="33%" />
         </Flex>
-        <FocusLock disabled={isOpen ? false : true} group="group2">
-          <Modal isOpen={isOpen} onClose={onClose} size="xl">
-            <ModalOverlay />
-            <ModalContent minW="50vw" bg="#262626">
-              <ModalCloseButton />
-              <ModalBody minW="50vw" h="100%" pt="40px">
-                <Carousel>
-                  <Flex h="100%" align="center">
-                    <Image src="/images/MicrosoftTeams-image.png" alt="image" />
-                  </Flex>
-                  <Flex h="100%" align="center">
-                    <Image
-                      src="/images/MicrosoftTeams-image (1) (1).png"
-                      alt="image"
-                    />
-                  </Flex>
-                  <Flex h="100%" align="center">
-                    <Image
-                      src="/images/MicrosoftTeams-image (2).png"
-                      alt="image"
-                    />
-                  </Flex>
-                  <Flex h="100%" align="center">
-                    <Image
-                      src="/images/MicrosoftTeams-image (3).png"
-                      alt="image"
-                    />
-                  </Flex>
-                  <Flex h="100%" align="center">
-                    <Image
-                      src="/images/MicrosoftTeams-image (5).png"
-                      alt="image"
-                    />
-                  </Flex>
-                </Carousel>
-              </ModalBody>
-            </ModalContent>
-          </Modal>
-        </FocusLock>
-      </Flex>
-      <Flex
-        w="100vw"
-        h="100vh"
-        bg="#262626"
-        overflowX="hidden"
-        direction="column"
-        overflow="hidden"
-        display={{base: "flex", lg: "none"}}
-      >
-        <Box overflowY="scroll" w="100%" h="100%" p="10px">
-          <Typist cursor={{ show: false }}>
-            <Text>Welcome!</Text>
+        <Flex
+          w="100%"
+          grow="1"
+          bg="#262626"
+          borderRadius={{base: "", lg: "0 0 10px 10px"}}
+          direction="column"
+          overflow="hidden"
+        >
+          <Box overflowY="scroll" w="100%" h="100%" p="10px">
+            <Typist cursor={{ show: false }}>
+              <Text>Welcome!</Text>
+              <br />
+              <Text>Type 'help' to view a list of commands</Text>
+              <Text>Or hit [Tab] to cycle through them</Text>
+            </Typist>
             <br />
-            <Text>Type 'help' to view a list of commands</Text>
-            <Text>Or hit [Tab] to cycle through them</Text>
-          </Typist>
-          <br />
-          {render.map((x, i) => (
-            <Box key={x + i}>{x}</Box>
-          ))}
-          <InputLine
-            input={input}
-            setInput={setInput}
-            handleSubmit={(e) => handleSubmit(e)}
-            setVisible={setVisible}
-            visible={visible}
-            options={Object.keys(content)}
-          />
-          <Box ref={endOfPage} />
-        </Box>
+            {render.map((x, i) => (
+              <Box key={x + i}>{x}</Box>
+            ))}
+            <InputLine
+              input={input}
+              setInput={setInput}
+              handleSubmit={(e) => handleSubmit(e)}
+              setVisible={setVisible}
+              visible={visible}
+              options={Object.keys(content)}
+            />
+            <Box ref={endOfPage} />
+          </Box>
+        </Flex>
       </Flex>
-    </>
+      <FocusLock disabled={isOpen ? false : true} group="group2">
+        <Modal isOpen={isOpen} onClose={onClose} size="xl">
+          <ModalOverlay />
+          <ModalContent minW="50vw" bg="#262626">
+            <ModalCloseButton />
+            <ModalBody minW="50vw" h="100%" pt="40px">
+              <Carousel>
+                <Flex h="100%" align="center">
+                  <Image src="/images/MicrosoftTeams-image.png" alt="image" />
+                </Flex>
+                <Flex h="100%" align="center">
+                  <Image
+                    src="/images/MicrosoftTeams-image (1) (1).png"
+                    alt="image"
+                  />
+                </Flex>
+                <Flex h="100%" align="center">
+                  <Image
+                    src="/images/MicrosoftTeams-image (2).png"
+                    alt="image"
+                  />
+                </Flex>
+                <Flex h="100%" align="center">
+                  <Image
+                    src="/images/MicrosoftTeams-image (3).png"
+                    alt="image"
+                  />
+                </Flex>
+                <Flex h="100%" align="center">
+                  <Image
+                    src="/images/MicrosoftTeams-image (5).png"
+                    alt="image"
+                  />
+                </Flex>
+              </Carousel>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </FocusLock>
+    </Flex>
   );
 }
